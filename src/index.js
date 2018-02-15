bf6667f236f0d5b4efb94a7499c82972b64f2e38
Dmitry Nutels <dnutels@users.noreply.github.com>
2018-02-15 08:55:46 +0200

    Added placeholder React and Semantic

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-import React, {Component} from "react";
+import React from 'react';
+import {render} from 'react-dom';
+
+import 'semantic-ui-css/semantic.min.css';
+
+import {Button} from 'semantic-ui-react';
+
+const styles = {
+    fontFamily: 'sans-serif',
+    textAlign: 'center'
+};
+
+const App = () => (
+    <div style={styles}>
+        <h2>Start editing to see some magic happen {'\u2728'}</h2>
+        <Button primary>Primary</Button>
+        <Button secondary>Secondary</Button>
+    </div>
+);
+
+render(<App />, document.getElementById('root'));
